3714bc4866bef841db00c0afc8374461e8707546
Gohith <gohithyeluri@gmail.com>
2026-06-28 20:57:42 +0530

    added

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing","web development","Full Stack Development"],
+    strings: ["Software Developer", "Java Development", "web designing"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
